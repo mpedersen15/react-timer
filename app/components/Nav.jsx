20fbe99058bd64e1b18ec9a8218d0602c08c21e1
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-14 07:16:26 -0600

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Nav = React.createClass({
+	render: function(){
+		return (		
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Timer</li>
+						<li>
+							<IndexLink to="/" activeClassName="active">Timer</IndexLink>
+						</li>
+						<li>
+							<Link to="/countdown" activeClassName="active">Countdown</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<ul className="menu">
+						<li className="menu-text">Created by <a href="">Matt Pedersen</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		);
+	}
+});  
+
+module.exports = Nav;
