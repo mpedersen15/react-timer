cbcec5cd95b115d9f0b38be3914eec3123f0e39a
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-15 19:45:46 -0600

    added react test utils, nav styling for active link

---
+++ app/components/Clock.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Clock = React.createClass({
+	render: function(){
+		<div></div>
+	}
+});
+
+module.exports = Clock;
---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,17 @@
+var React = require('react'),
+ReactDOM = require('react-dom'),
+expect= require('expect'),
+$ = require('jQuery'),
+TestUtils = require('react-addons-test-utils');
+
+var Clock = require('Clock');
+
+describe('Clock', () => {
+	it('should exist', () => {
+		expect(Clock).toExist();
+	});
+	
+	/* describe('formatSeconds', () => {
+		
+	}); */
+});
