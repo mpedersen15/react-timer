99290d2dcee15f6829346dab3946158d9b0130a4
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-05-19 15:51:05 -0600

    added handleSetCountdown tests

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -10,4 +10,35 @@ describe('Countdown', () => {
 	it('should exist', () => {
 		expect(Countdown).toExist();
 	});
+
+	describe('handleSetCountdown', () => {
+		it('should start countdown', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
+			countdown.handleSetCountdown(10);
+
+			expect(countdown.state.count).toBe(10);
+			expect(countdown.state.countdownStatus).toBe('started');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(9);
+			},1001);
+
+		});
+
+		it('should not let count less than 0', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
+			countdown.handleSetCountdown(1);
+
+			expect(countdown.state.count).toBe(1);
+			expect(countdown.state.countdownStatus).toBe('started');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(0);
+			},3001);
+
+		});
+
+	});
 });
